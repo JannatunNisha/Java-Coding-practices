370c0472a603ca122f2003c861662b3bae31ffef
Jannatun Nisha <jannatunnisha6203@gmail.com>
2026-07-15 02:00:24 +0530

    Rewrite the code

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+import java util.scannar.*;
+
+public class first {
+    System.out.println("Hello World!");
+    
+}
--- first.java
+++
@@ -1,6 +0,0 @@
-package Java-Coding-practices;
-
-public class first code {
-    System.out.println(Hello World!);
-    
-}
